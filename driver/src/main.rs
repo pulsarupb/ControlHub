4500c2e88215d39e84c708f46a59b2fd773d821e
lazar <dasidasi86@gmail.com>
2026-05-22 16:34:33 +0300

    feat: scan for can devices

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use moteus::transport::args::TransportArgs;
+use moteus::transport::singleton::get_singleton_transport;
+use moteus::{Transport, TransportOptions};
+
+fn main() -> Result<(), moteus::Error> {
+    let args = TransportOptions::new()
+        .transport_args(TransportArgs::new().with_can_interface("can0".to_string()));
+
+    let transport = get_singleton_transport(Some(&args))?;
+    let mut transport = transport.lock().unwrap();
+
+    let devices = transport.discover(0, 0)?;
+
+    if devices.is_empty() {
+        println!("No devices found!");
+        return Ok(());
+    }
+
+    for device in &devices {
+        println!("{}", device);
+    }
+
+    Ok(())
 }
