1992f773bff20c7b73b3149c0680ddb3990744a7
lazar <dasidasi86@gmail.com>
2026-05-28 12:40:46 +0300

    feat: simple 4 motor drive

--- commands.sh
+++
@@ -1,4 +0,0 @@
-sudo ip link set can0 down
-sudo ip link set can0 type can bitrate 1000000 dbitrate 5000000 fd on sample-point 0.666 dsample-point 0.666 restart-ms 100 berr-reporting on
-sudo ip link set can0 up
-ip -details -statistics link show can0
--- driver/src/main.rs
+++ driver/src/main.rs
@@ -4,32 +4,7 @@ use moteus::{
 };
 use std::time::Duration;
 fn main() -> Result<(), moteus::Error> {
-    // let mut found = Vec::new();
-    // for id in 0u8..=6 {
-    //     print!("Trying ID {}... ", id);
-    //     match BlockingController::new(id) {
-    //         Ok(mut ctrl) => match ctrl.set_stop() {
-    //             Ok(result) => {
-    //                 println!(
-    //                     "FOUND mode={:?} pos={:.4} vel={:.4} fault={}",
-    //                     result.mode, result.position, result.velocity, result.fault
-    //                 );
-
-    //                 found.push(id);
-    //             }
-    //             Err(err) => {
-    //                 println!("no reply ({})", err);
-    //             }
-    //         },
-    //         Err(err) => {
-    //             println!("transport error ({})", err);
-    //         }
-    //     }
-    // }
-    // println!();
-    // println!("Found moteus IDs: {:?}", found);
-
-    let mut result1 = BlockingController::new(0);
+    let mut result1 = BlockingController::new(1);
     let mut ctrl1 = match result1 {
         Ok(ctrl) => ctrl,
         Err(err) => {
@@ -39,7 +14,7 @@ fn main() -> Result<(), moteus::Error> {
     };
     ctrl1.set_stop()?;
 
-    let mut result2 = BlockingController::new(1);
+    let mut result2 = BlockingController::new(2);
     let mut ctrl2 = match result2 {
         Ok(ctrl) => ctrl,
         Err(err) => {
@@ -50,7 +25,7 @@ fn main() -> Result<(), moteus::Error> {
 
     ctrl2.set_stop()?;
 
-    let mut result3 = BlockingController::new(2);
+    let mut result3 = BlockingController::new(3);
     let mut ctrl3 = match result3 {
         Ok(ctrl) => ctrl,
         Err(err) => {
@@ -72,37 +47,29 @@ fn main() -> Result<(), moteus::Error> {
 
     loop {
         let mut command1 = PositionCommand::new();
-        command1.velocity = Some(1.0);
+        command1.position = Some(f32::NAN);
+        command1.velocity = Some(5.0);
         command1.watchdog_timeout = Some(0.5);
 
         let mut command2 = PositionCommand::new();
-        command2.velocity = Some(1.0);
+        command2.position = Some(f32::NAN);
+        command2.velocity = Some(5.0);
         command2.watchdog_timeout = Some(0.5);
 
         let mut command3 = PositionCommand::new();
-        command3.velocity = Some(1.0);
+        command3.position = Some(f32::NAN);
+        command3.velocity = Some(5.0);
         command3.watchdog_timeout = Some(0.5);
 
         let mut command4 = PositionCommand::new();
-        command4.velocity = Some(1.0);
+        command4.position = Some(f32::NAN);
+        command4.velocity = Some(5.0);
         command4.watchdog_timeout = Some(0.5);
 
         let result1 = ctrl1.set_position(command1);
         let result2 = ctrl2.set_position(command2);
         let result3 = ctrl3.set_position(command3);
         let result4 = ctrl4.set_position(command4);
-
-        match result1 {
-            Ok(result) => {
-                println!(
-                    "Controller 1 - mode={:?} pos={:.4} vel={:.4} fault={}",
-                    result.mode, result.position, result.velocity, result.fault
-                );
-            }
-            Err(err) => {
-                println!("Error sending position command to controller 1: {}", err);
-            }
-        }
 
         match result2 {
             Ok(result) => {
